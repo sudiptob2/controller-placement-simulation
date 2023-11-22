b7408a3a165cc0893a08a75f3ddc2c9f0221cfcb
sudipto baral <sudiptobaral.me@gmail.com>
2023-11-22 17:52:19 -0500

    docs: updated comment in main.py
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import graph_mixin
 
 if __name__ == '__main__':
+    # visualize available networks
     graph_raw = ['network1', 'network2']
     for i in range(len(graph_raw)):
         file_name = graph_raw[i]+'.txt'
